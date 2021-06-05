ab5e10664f1d36185ca532c015a0c1ec1ee9c7f8
quynhethereal <49337640+quynhethereal@users.noreply.github.com>
2021-06-05 21:51:32 +0700

    lesson 4

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import './App.css';
 
 import { HomePage } from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import { Link, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
         <Route path="/shop" component={ShopPage}></Route>
+        <Route path="/signin" component={SignInAndSignUpPage}></Route>
       </Switch>
     </div>
   );
