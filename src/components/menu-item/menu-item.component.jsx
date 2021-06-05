fdb1dd43d93b024ef70a77637f9a997edc046965
quynhethereal <49337640+quynhethereal@users.noreply.github.com>
2021-06-05 10:17:23 +0700

    lesson 2

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import '../menu-item/menu-item.styles.scss'
+
+export const MenuItem = ({title,imageURL,size}) => (
+    <div  className = {`${size} menu-item`}>
+        <div className="background-image" style={{backgroundImage:`url(${imageURL})`}}>
+
+        </div>
+    <div className = "content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+    </div>
+</div>
+)
